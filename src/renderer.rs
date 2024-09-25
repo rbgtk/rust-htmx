f3d6ce4ef52ea2c518a85aae9b69bfb2e3f451b1
rbgtk <rbgtk>
2024-09-25 21:53:48 +0200

    rust handlebars

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,7 +4,13 @@ use handlebars::Handlebars;
 pub fn create() -> Engine<Handlebars<'static>> {
     let mut hbs = Handlebars::new();
 
-    hbs.register_template_file("index", "templates/index.hbs").unwrap();
+    hbs.register_template_file("layout", "views/layouts/default.hbs").unwrap();
+
+    hbs.register_template_file("index", "views/index.hbs").unwrap();
+    hbs.register_template_file("about", "views/about.hbs").unwrap();
+    hbs.register_template_file("users", "views/user/list.hbs").unwrap();
+
+    hbs.register_template_file("nav", "views/partials/nav.hbs").unwrap();
 
     Engine::from(hbs)
 }
